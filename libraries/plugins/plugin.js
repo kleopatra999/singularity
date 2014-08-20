f4afb840c75a333f591780b96c0b0b157f9d5bd3
chr0n1x <heilong24@gmail.com>
2014-08-19 22:37:03 -0400

    plugins/plugin: base functions for handling config change requests

--- libraries/plugins/plugin.js
+++ libraries/plugins/plugin.js
@@ -1,5 +1,19 @@
 "use strict";
 
 module.exports = require('../vent').extend({
-  objectType: 'plugin'
+  // **SHOULD** be swapped out with the adapter.name that this plugin
+  // is attached to
+  objectType: 'plugin',
+
+  removeConfig: function(httpPayload) {
+    this.error('not set up to remove config', this.logForObject(httpPayload));
+  },
+
+  addConfig: function(httpPayload) {
+    this.error('not set up to add config', this.logForObject(httpPayload));
+  },
+
+  updateConfig: function(httpPayload) {
+    this.error('not set up to update config', this.logForObject(httpPayload));
+  }
 });
